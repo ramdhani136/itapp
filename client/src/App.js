240936a892833e19907fb2153c2e3eb8b518c02d
Ilham Ramdhani <ramdhaniit@gmail.com>
2021-01-26 12:36:00 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ilham Ramdhani
         </p>
         <a
           className="App-link"
